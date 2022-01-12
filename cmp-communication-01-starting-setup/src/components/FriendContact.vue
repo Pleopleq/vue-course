b8e6453acd48c0484ec65309cc4b19da8232b889
Felix Anducho <felix.anducho74@gmail.com>
2022-01-11 21:07:46 -0600

    Working with props and child -> parent comunication(custom events)

---
+++ cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ favorite ? '(Favorite)' : '' }} </h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  props: {
+    id: String,
+    name: String,
+    phone: String,
+    email: String,
+    favorite: Boolean
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit('toggle-favorite', this.id);
+    }
+  }
+};
+</script>
