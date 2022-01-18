b14057ad423b48df162f849f89ebace8c9b4552c
Felix Anducho <felix.anducho74@gmail.com>
2022-01-17 21:05:59 -0600

    Add initial files for 'learning resources app' project

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <ul v-for="resource in this.resources" :key="resource.id">
+      <resource
+        :title="resource.title"
+        :description="resource.description"
+        :link="resource.link"
+      ></resource>
+  </ul>
+</div>
+</template>
+
+<script>
+import Resource from './components/ResourceItem.vue'
+export default {
+  components: { Resource },
+    data() {
+        return {
+            resources: [
+                {
+                    id: 1,
+                    title: 'Offical Guide',
+                    description: 'The official Vue.js documentation',
+                    link:'https://vuejs.org'
+                },
+                {
+                    id: 2,
+                    title: 'Google',
+                    description: 'Learn to google',
+                    link:'https://google.com'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ prj-cmp-01-starting-setup/src/components/ResourceItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <li>
+        <div>
+            <header>
+                <h2>{{title}}</h2>
+                <button>Delete</button>
+            </header>
+            <div>
+                <p>{{description}}</p>
+                <a :href="link" target="_blank">View Resource</a>
+            </div>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'Resource',
+    props: {
+        title: String,
+        description: String,
+        link: String
+    }
+}
+</script>
+
+<style>
+
+</style>
