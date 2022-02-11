91c232ab09f531dcfe7e54dbfe31ee1c8b10ddac
Felix Anducho <felix.anducho74@gmail.com>
2022-02-10 21:00:43 -0600

    Add computed properties to coachItems to control link to details and contact

--- main-prj-01-starting-setup/src/components/coaches/CoachItem.vue
+++ main-prj-01-starting-setup/src/components/coaches/CoachItem.vue
@@ -4,13 +4,22 @@
     <span v-for="role in roles" :key="role">{{ role }}</span>
     <p>Rating: {{ rate }}</p>
     <router-view></router-view>
-    <router-link to="/coaches/c1/contact">Contact</router-link>
+    <router-link :to="coachContactLink">Contact</router-link>
+    <router-link :to="coachDetailsLink">View Details</router-link>
   </base-card>
 </template>
 
 <script>
 export default {
   props: ['id', 'firstName', 'lastName', 'rate', 'roles'],
+  computed: {
+    coachContactLink() {
+      return '/coaches/' + this.id + '/contact';
+    },
+    coachDetailsLink() {
+      return '/coaches/' + this.id;
+    },
+  },
 };
 </script>
 
